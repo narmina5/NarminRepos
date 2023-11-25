f6946790e0998706e7869da4f40e300e79801c0a
NARMIN <narminm@email.com>
2023-11-25 14:27:49 +0100

    product app created, multilanguage added

---
+++ app/product/translation.py
@@ -0,0 +1,11 @@
+from modeltranslation.translator import TranslationOptions, translator
+
+from .models import Category
+
+
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+translator.register(Category, CategoryTranslationOptions)
+
+
